99062d674cacbd5cbf57136838b470604145c064
Daniel Bartholomae <daniel@bartholomae.name>
2021-04-30 13:55:02 +0200

    feat: show inquiry info after entering email

--- src/InquiryView.test.tsx
+++ src/InquiryView.test.tsx
@@ -4,6 +4,7 @@ import { providersContext } from "./providers/providersContext";
 import { MockProvider } from "./providers/MockProvider";
 import { Provider } from "./providers/Provider";
 import { InquiryView } from "./InquiryView";
+import { enterEmail } from "./EmailForm.po";
 
 describe("InquiryView", () => {
   let mockProvider: Provider;
@@ -17,7 +18,14 @@ describe("InquiryView", () => {
     );
   });
 
-  it("shows the information tracked about me", async () => {
-    expect(screen.getByText("MockProvider")).toBeInTheDocument();
+  describe("when I enter my email", () => {
+    beforeEach(() => {
+      const email = "test@test.com";
+      enterEmail(email);
+    });
+
+    it("shows the information tracked about me", async () => {
+      expect(screen.getByText(mockProvider.name)).toBeInTheDocument();
+    });
   });
 });
--- src/InquiryView.tsx
+++ src/InquiryView.tsx
@@ -1,16 +1,23 @@
-import React, { FunctionComponent, useContext } from "react";
+import React, { FunctionComponent, useContext, useState } from "react";
 import { Container, Typography } from "@material-ui/core";
 import { providersContext } from "./providers/providersContext";
+import { EmailForm } from "./EmailForm";
 
 export const InquiryView: FunctionComponent = () => {
+  const [email, setEmail] = useState<string | null>(null);
   const providers = useContext(providersContext);
   return (
     <Container>
-      {providers.map((provider) => (
-        <Typography variant="h3" gutterBottom>
-          {provider.name}
-        </Typography>
-      ))}
+      <Typography variant="h2" gutterBottom>
+        Inquiries
+      </Typography>
+      <EmailForm onSetEmail={setEmail} />
+      {email != null &&
+        providers.map((provider) => (
+          <Typography variant="h3" gutterBottom>
+            {provider.name}
+          </Typography>
+        ))}
     </Container>
   );
 };
