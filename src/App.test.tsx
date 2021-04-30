bffa6394ca84f0c70561a9aa0b800007150d883a
Daniel Bartholomae <daniel@bartholomae.name>
2021-04-30 11:12:11 +0200

    feat: add components to sign up email

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import userEvent from "@testing-library/user-event";
 
-test('shows a title', () => {
-  render(<App />);
-  expect(screen.getByText('Data collection')).toBeInTheDocument();
+describe("App", () => {
+  it("shows a title", () => {
+    render(<App />);
+    expect(screen.getByText("Data collection")).toBeInTheDocument();
+  });
+
+  it("allows to sign up an email", () => {
+    render(<App />);
+    const input = screen.getByLabelText("Email");
+    userEvent.type(input, "test@test.com");
+    userEvent.click(screen.getByRole("button", { name: "Sign up" }));
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "fontsource-roboto";
 import React from "react";
-import { Container, Typography } from "@material-ui/core";
+import { Button, Container, TextField, Typography } from "@material-ui/core";
 
 function App() {
   return (
@@ -8,6 +8,12 @@ function App() {
       <Typography variant="h2" gutterBottom>
         Data collection
       </Typography>
+      <form style={{ display: "flex", gap: "4px" }}>
+        <TextField id="email" label="Email" />
+        <Button variant="contained" color="primary">
+          Sign up
+        </Button>
+      </form>
     </Container>
   );
 }
